a3288d2dd07ae0289a68b087835e7aa5d78659bd
Antoine Couchard <couchardantoine@gmail.com>
2019-10-13 11:37:10 +0200

    Fix voiceutil doc

--- voiceutil/doc.go
+++ voiceutil/doc.go
@@ -1,6 +1,6 @@
 /*
 Package voiceutil provides utilities to work with harmony voice
 connections. It contains adapters that do the conversion between
-PCM and Opus data.
+PCM and Opus-encoded data.
 */
 package voiceutil
