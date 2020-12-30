b7345a7256b55222a99f8c7a57a858f98218fb8f
Antoine Couchard <couchardantoine@gmail.com>
2020-12-30 12:38:33 +0100

    Verify client has intents before registering handlers

--- discord/gateway_intent.go
+++ discord/gateway_intent.go
@@ -3,6 +3,11 @@ package discord
 // GatewayIntent specifies which events the Gateway should send to a client.
 type GatewayIntent int
 
+// String implements fmt.Stringer.
+func (i GatewayIntent) String() string {
+	return intentNames[i]
+}
+
 // List of gateway intents a client can subscribe to.
 const (
 	GatewayIntentGuild                  GatewayIntent = 1 << 0
@@ -24,3 +29,21 @@ const (
 
 // Equivalent to all intents except privileged (GatewayIntentGuildMembers and GatewayIntentGuildPresences), OR'd.
 const GatewayIntentUnprivileged = GatewayIntentGuild | GatewayIntentGuildBans | GatewayIntentGuildEmojis | GatewayIntentGuildIntegrations | GatewayIntentGuildWebhooks | GatewayIntentGuildInvites | GatewayIntentGuildVoiceStates | GatewayIntentGuildMessages | GatewayIntentGuildMessageReactions | GatewayIntentGuildMessageTyping | GatewayIntentDirectMessages | GatewayIntentDirectMessageReactions | GatewayIntentDirectMessageTyping
+
+var intentNames = map[GatewayIntent]string{
+	GatewayIntentGuild:                  "GUILDS",
+	GatewayIntentGuildMembers:           "GUILD_MEMBERS",
+	GatewayIntentGuildBans:              "GUILD_BANS",
+	GatewayIntentGuildEmojis:            "GUILD_EMOJIS",
+	GatewayIntentGuildIntegrations:      "GUILD_INTEGRATIONS",
+	GatewayIntentGuildWebhooks:          "GUILD_WEBHOOKS",
+	GatewayIntentGuildInvites:           "GUILD_INVITES",
+	GatewayIntentGuildVoiceStates:       "GUILD_VOICE_STATES",
+	GatewayIntentGuildPresences:         "GUILD_PRESENCES",
+	GatewayIntentGuildMessages:          "GUILD_MESSAGES",
+	GatewayIntentGuildMessageReactions:  "GUILD_MESSAGE_REACTIONS",
+	GatewayIntentGuildMessageTyping:     "GUILD_MESSAGE_TYPING",
+	GatewayIntentDirectMessages:         "DIRECT_MESSAGES",
+	GatewayIntentDirectMessageReactions: "DIRECT_MESSAGE_REACTIONS",
+	GatewayIntentDirectMessageTyping:    "DIRECT_MESSAGE_TYPING",
+}
